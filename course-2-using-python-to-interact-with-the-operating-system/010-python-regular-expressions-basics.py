3f860612bd34e421c1c03aaad9a014d331f6216d
Arjamand Ali <140328490+arjamand@users.noreply.github.com>
2024-07-02 15:51:32 +0500

    regex basics

--- course-2-using-python-to-interact-with-the-operating-system/010-python-regular-expressions-basics.py
+++ course-2-using-python-to-interact-with-the-operating-system/010-python-regular-expressions-basics.py
@@ -2,6 +2,18 @@
 
 import re
 
-exp_search = re.search(r"a", "Arjamand Ali", re.IGNORECASE())
-
+exp_search = re.search(r"a", "Arjamand Ali")
 print(exp_search)
+
+print(re.search(r"Py.*n", "Pygmalion"))
+print(re.search(r"Py.*n", "Python Programming"))
+print(re.search(r"Py[a-z]*n", "Python Programming"))
+print(re.search(r"Py[a-z]*n", "Pyn"))
+
+print(re.search(r"o+l+", "goldfish"))
+print(re.search(r"o+l+", "woolly"))
+print(re.search(r"o+l+", "boil"))
+
+print(re.search(r"p?each", "To each their own"))
+print(re.search(r"p?each", "I like peaches"))
+
