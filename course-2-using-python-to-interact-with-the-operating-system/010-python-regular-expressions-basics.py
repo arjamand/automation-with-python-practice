044f62602c4ad2e46da41d155b59d219778f1da4
Arjamand Ali <140328490+arjamand@users.noreply.github.com>
2024-07-03 20:48:02 +0500

    regex basics

--- course-2-using-python-to-interact-with-the-operating-system/010-python-regular-expressions-basics.py
+++ course-2-using-python-to-interact-with-the-operating-system/010-python-regular-expressions-basics.py
@@ -5,6 +5,8 @@ import re
 exp_search = re.search(r"a", "Arjamand Ali")
 print(exp_search)
 
+
+#repitition
 print(re.search(r"Py.*n", "Pygmalion"))
 print(re.search(r"Py.*n", "Python Programming"))
 print(re.search(r"Py[a-z]*n", "Python Programming"))
@@ -17,3 +19,11 @@ print(re.search(r"o+l+", "boil"))
 print(re.search(r"p?each", "To each their own"))
 print(re.search(r"p?each", "I like peaches"))
 
+#escapeing characters
+
+print(re.search(r".com", "welcome"))
+print(re.search(r"\.com", "welcome"))
+print(re.search(r"\.com", "mydomain.com"))
+
+print(re.search(r"\w*", "This is an example"))
+print(re.search(r"\w*", "And_this_is_another"))
